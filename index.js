f8b452e5cb557bf97ce61f8d32eb39eaaef3c4c1
Jamal O'Garro <jamal.ogarro@gmail.com>
2021-06-25 12:15:07 -0400

    Update deps

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+app.use(helmet());
+app.use(express.static(__dirname + '/public'));
 
 console.log("Server Running on " + PORT);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(helmet());
-  app.use(express.static("public"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
